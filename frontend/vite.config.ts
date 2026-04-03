3c3d419ad63c1d78fce6a82cdf65be6d9a2e5dff
UWIZEYIMANA Jean Pierre <132813652+ujpm@users.noreply.github.com>
2026-04-03 08:01:02 +0000

    Configuring the Vite Proxy and Environment Variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,25 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+
+  server: {
+    proxy: {
+      // Any request starting with /api will be intercepted by Vite
+      '/api': {
+        target: 'https://glucolens-backend.onrender.com',
+        changeOrigin: true, // This is what tricks the CORS policy
+        secure: false,      // Set to false to avoid SSL cert errors in Codespaces
+        
+        // to see the proxy working in our terminal
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (_proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+        }
+      }
+    }
+  }
 })
